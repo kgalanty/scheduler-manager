ac5c7ba9b3165cb3e4750b1cbac95e8771b5a011
kgalanty <dataz.karas@gmail.com>
2024-01-20 21:12:52 +0100

    add title

--- scheduler/vue.config.js
+++ scheduler/vue.config.js
@@ -1,29 +1,37 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Schedule Manager',
+    },
+  },
   publicPath: ".",
- //devServer: {
-   // https: {
-   //   key: fs.readFileSync('./certs/example.com+5-key.pem'),
-   //   cert: fs.readFileSync('./certs/example.com+5.pem'),
-   // },
-   //  public: 'https://localhost:8080/',
-   //  disableHostCheck: true,
-   // disableHostCheck: true,
-   devServer: {
-     // open: process.platform === 'darwin',
-     // host: '0.0.0.0',
-     // port: 8080, // CHANGE YOUR PORT HERE!
-     // https: true,
-     // hotOnly: false,
-     //public: 'https://localhost:8080/',
-      
-   proxy: {
-     "^/scheduleapi": {
-       target: "https://my.tmdhosting.com/schedule",
-       logLevel: "debug",
-       changeOrigin: true,
-       secure: true,
-       withCredentials: true,
-       pathRewrite: { "^/scheduleapi": "/scheduleapi" }
-     }
-   }
- }}
+  //devServer: {
+  // https: {
+  //   key: fs.readFileSync('./certs/example.com+5-key.pem'),
+  //   cert: fs.readFileSync('./certs/example.com+5.pem'),
+  // },
+  //  public: 'https://localhost:8080/',
+  //  disableHostCheck: true,
+  // disableHostCheck: true,
+  devServer: {
+    // open: process.platform === 'darwin',
+    // host: '0.0.0.0',
+    // port: 8080, // CHANGE YOUR PORT HERE!
+    // https: true,
+    // hotOnly: false,
+    //public: 'https://localhost:8080/',
+
+    proxy: {
+      "^/scheduleapi": {
+        target: "https://staging.tmd-clientarea2023.info/schedule",
+        logLevel: "debug",
+        changeOrigin: true,
+        secure: true,
+        withCredentials: true,
+        pathRewrite: { "^/scheduleapi": "/scheduleapi" }
+      }
+    }
+  }
+}
